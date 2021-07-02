1b7be2eb0d277e97604c3de2aa04754d52edef98
AshleySetter <assetter@hotmail.com>
2021-07-02 21:17:01 +0100

    added support for external links using the Links component. header with

--- src/components/link.js
+++ src/components/link.js
@@ -3,16 +3,28 @@ import PropTypes from "prop-types";
 import {Anchor} from "grommet";
 import {navigate} from "gatsby";
 
-const Link = ({to, ...rest}) => (
-  <Anchor
-    href={to}
-    onClick={(ev) => {
-      navigate(to);
-      ev.preventDefault();
-    }}
-    {...rest}
-  />
-);
+const Link = ({to, children, ...rest}) => {
+  const internal = /^\/(?!\/)/.test(to);
+  let content = (
+    <a href={to} {...rest}>
+      {children}
+    </a>
+  );
+  if (internal) {
+    content = (
+      <Anchor
+        href={to}
+        onClick={(ev) => {
+          navigate(to);
+          ev.preventDefault();
+        }}
+      >
+        {children}
+      </Anchor>
+    );
+  }
+  return content;
+};
 
 Link.propTypes = {
   to: PropTypes.string,
