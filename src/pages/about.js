d8ee7db475874df04fb5d7fc926a42733e8fe895
AshleySetter <assetter@hotmail.com>
2021-07-03 09:15:36 +0100

    moved cat pictures to about page, added pic of Tux

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,30 @@
 import React from "react";
 import Layout from "../components/layout";
+import {StaticImage} from "gatsby-plugin-image";
+import {Box, Card} from "grommet";
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>
+        Hi there, my name is Ashley,
+        I'm a software developer and previously a physicist and this is my website.
+        My partner Heather and I own 2 lovely cats which may be seen below.
+      </p>
+      <Box direction="row" wrap>
+        <Card height="medium" width="medium">
+          <StaticImage
+            alt="Jinx a black cat, lying on a window ledge in the sun and looking up at the camera."
+            src="../images/Jinx.jpg"
+          />
+        </Card>
+        <Card height="medium" width="medium">
+          <StaticImage
+            alt="Tux a black and white cat, sitting on a window ledge, looking up at the camera."
+            src="../images/Tux.jpg"
+          />
+        </Card>
+      </Box>
     </Layout>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,11 @@
 import React from "react";
 import Layout from "../components/layout";
-import {StaticImage} from "gatsby-plugin-image";
 
 // markup
 const IndexPage = () => {
   return (
     <Layout pageTitle="Welcome to my Site">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <div>
-        <StaticImage
-          alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera."
-          src="../images/Jinx.jpg"
-        />
-      </div>
-      <div> Some more content after the image </div>
+      <p>Welcome to my humble website.</p>
     </Layout>
   );
 };
